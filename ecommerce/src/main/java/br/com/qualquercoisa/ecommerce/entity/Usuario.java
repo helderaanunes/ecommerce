5a1a5e7a313aa65eceb8b7286bea993f9fe55f83
Laurindo Neto <laurindo1409@gmail.com>
2024-09-18 06:52:28 -0300

    Update Usuario.java
    Repository

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/Usuario.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/Usuario.java
@@ -1,10 +1,12 @@
 package br.com.qualquercoisa.ecommerce.entity;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
-
-@Data
 @Entity
+@Data
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
