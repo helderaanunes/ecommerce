3218b8c5e746fc606cdc4142590196ef3634209f
guiza01 <jefersonguilherme.pe@gmail.com>
2024-09-18 17:42:27 -0300

    Service ProdutoEstoqueService

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/ProdutoEstoqueService.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/ProdutoEstoqueService.java
@@ -1,4 +1,45 @@
 package br.com.qualquercoisa.ecommerce.service;
 
+import br.com.qualquercoisa.ecommerce.entity.ProdutoEstoque;
+import br.com.qualquercoisa.ecommerce.repository.ProdutoEstoqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProdutoEstoqueService {
+
+    @Autowired
+    private ProdutoEstoqueRepository produtoEstoqueRepository;
+
+    public List<ProdutoEstoque> listarTodos() {
+        return (List<ProdutoEstoque>) produtoEstoqueRepository.findAll();
+    }
+
+    public Optional<ProdutoEstoque> buscarPorId(Long id) {
+        return produtoEstoqueRepository.findById(id);
+    }
+
+    public ProdutoEstoque salvar(ProdutoEstoque produtoEstoque) {
+        return produtoEstoqueRepository.save(produtoEstoque);
+    }
+
+    public ProdutoEstoque atualizar(Long id, ProdutoEstoque produtoEstoqueAtualizado) {
+        Optional<ProdutoEstoque> produtoEstoqueExistente = produtoEstoqueRepository.findById(id);
+        if (produtoEstoqueExistente.isPresent()) {
+            ProdutoEstoque produtoEstoque = produtoEstoqueExistente.get();
+            produtoEstoque.setProduto(produtoEstoqueAtualizado.getProduto());
+            produtoEstoque.setPreco(produtoEstoqueAtualizado.getPreco());
+            produtoEstoque.setEstoque(produtoEstoqueAtualizado.getEstoque());
+            return produtoEstoqueRepository.save(produtoEstoque);
+        } else {
+            throw new RuntimeException("Produto em Estoque não encontrado com o ID: " + id);
+        }
+    }
+
+    public void deletar(Long id) {
+        produtoEstoqueRepository.deleteById(id);
+    }
 }
