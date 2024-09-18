d2fda61f57eacc16fab5c55c39c8c7418805a614
guiza01 <jefersonguilherme.pe@gmail.com>
2024-09-18 17:06:00 -0300

    Update ProdutoEstoqueRepository.java

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/repository/ProdutoEstoqueRepository.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/repository/ProdutoEstoqueRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoEstoquerepository extends CrudRepository<ProdutoEstoque, Long> {
+public interface ProdutoEstoqueRepository extends CrudRepository<ProdutoEstoque, Long> {
 }
