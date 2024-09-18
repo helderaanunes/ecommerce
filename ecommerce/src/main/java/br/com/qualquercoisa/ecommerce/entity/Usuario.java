0708258d69b4278b17167d7701626a6f26315493
guiza01 <jefersonguilherme.pe@gmail.com>
2024-09-18 17:34:49 -0300

    Atualizações e Service TransportadoraService

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/Usuario.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/Usuario.java
@@ -1,8 +1,5 @@
 package br.com.qualquercoisa.ecommerce.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -12,12 +9,12 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    @ManyToOne
     private String cpf;
     private String rg;
     private String login;
     private String senha;
     private String email;
     private String telefone;
-    private permissao Permissao;
+    @ManyToOne
+    private Permissao permissao;
 }
--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/repository/UsuarioRepository.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/repository/UsuarioRepository.java
@@ -3,6 +3,6 @@ import br.com.qualquercoisa.ecommerce.entity.Usuario;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@repository
+@Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 }
