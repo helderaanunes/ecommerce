db692a7aae8e08ebdbf178653308a3724d39bb0e
Helder <helder.nunes@afogados.ifpe.edu.br>
2024-09-17 21:07:28 -0300

    Entidade Categoria

---
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/Categoria.java
@@ -0,0 +1,15 @@
+package br.com.qualquercoisa.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Categoria categoria;
+}
