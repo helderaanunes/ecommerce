ae875841097bd4b7a63f71f8ad542256d1acbd87
Bruno Sena <senabruno96@gmail.com>
2024-09-21 14:36:26 -0300

    FluxoFinanceiro Entity Repository

---
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/FluxoFinanceiro.java
@@ -0,0 +1,24 @@
+package br.com.qualquercoisa.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+
+public class FluxoFinanceiro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date dataVencimento;
+    private Date dataPagamento;
+    private Float valor;
+    private String descricao;
+    private String fluxo;
+    @ManyToOne
+    private Venda venda;
+
+
+}
