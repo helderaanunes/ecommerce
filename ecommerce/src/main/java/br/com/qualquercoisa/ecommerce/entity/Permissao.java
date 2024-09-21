ffd5d282798e325d789f5e97d74f75b73ae6477a
Allyson-Balbino <allysonbalbino@gmail.com>
2024-09-21 13:05:20 -0300

    Update Permissao.java
    construtor que inicialize o campo nome; validação com @NotBlank do pacote jakarta.validation para garantir que o campo nome não seja nulo

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/Permissao.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/entity/Permissao.java
@@ -1,8 +1,22 @@
 package br.com.qualquercoisa.ecommerce.entity;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Entity
+@Data
+@NoArgsConstructor
 public class Permissao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotBlank(message = "Nome não pode ser vazio")
+    private String nome;
+
+    public Permissao(String nome) {
+        this.nome = nome;
+    }
 }
