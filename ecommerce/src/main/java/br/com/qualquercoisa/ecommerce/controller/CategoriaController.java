31ada76b245129daa269711a87ffe748aa576a11
Helder <helder.nunes@afogados.ifpe.edu.br>
2024-09-24 21:54:45 -0300

    Categoria serv e cont

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/CategoriaController.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/CategoriaController.java
@@ -1,4 +1,24 @@
 package br.com.qualquercoisa.ecommerce.controller;
 
+import br.com.qualquercoisa.ecommerce.entity.Categoria;
+import br.com.qualquercoisa.ecommerce.repository.CategoriaRepository;
+import br.com.qualquercoisa.ecommerce.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @PostMapping("/categoria")
+    public ResponseEntity<Categoria> salvar (@RequestBody Categoria categoria){
+        return categoriaService.salvar(categoria);
+    }
+
 }
