e660a4b088617c8f28760a97dc3be09d078e5673
Carlitos <chqc@discente.ifpe.edu.br>
2024-10-01 21:13:48 -0300

    Update EstoqueService.java

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/EstoqueService.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/EstoqueService.java
@@ -1,7 +1,10 @@
 package br.com.qualquercoisa.ecommerce.service;
 
+import br.com.qualquercoisa.ecommerce.entity.Categoria;
 import br.com.qualquercoisa.ecommerce.entity.Estoque;
 //import br.com.qualquercoisa.ecommerce.repository.EstoqueRepository;
+import br.com.qualquercoisa.ecommerce.repository.CategoriaRepository;
+import br.com.qualquercoisa.ecommerce.repository.EstoqueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +19,21 @@ public class EstoqueService {
    @Autowired
    private EstoqueRepository estoqueRepository;
 
+   public Iterable<Estoque> listarTodos (){
+      return estoqueRepository.findAll();
+   }
 
-   public ResponseEntity<Estoque> salvar (Estoque estoque) {
-      Estoque estoqueJaSalva = estoqueRepository.save(estoque);
-      return new ResponseEntity<Estoque>(estoqueJaSalva, HttpStatus.OK);
+   public ResponseEntity<Estoque> salvar (Estoque estoque){
+      return new ResponseEntity<Estoque>(estoqueRepository.save(estoque), HttpStatus.OK);
+   }
 
+   public ResponseEntity<Estoque> buscarPorId(Long id) {
+      return new ResponseEntity<Estoque>(estoqueRepository.findById(id).orElseThrow(),HttpStatus.OK);
+   }
 
+   public ResponseEntity deletar(Long id) {
+      estoqueRepository.deleteById(id);
+      return new ResponseEntity("{\"mensagem\":\"Removido com sucesso\"}",HttpStatus.OK);
    }
 
 
