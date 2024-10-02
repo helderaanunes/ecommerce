f31a4617668b7c596cc911f99bc1cce2c73bb57d
Nayara <vitorianayy6@gmail.com>
2024-10-01 21:24:04 -0300

    IMAGEMPRODUTOSERVICE E CONTROLLER

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/ImagemProdutoController.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/ImagemProdutoController.java
@@ -1,24 +1,44 @@
 package br.com.qualquercoisa.ecommerce.controller;
 import br.com.qualquercoisa.ecommerce.entity.ImagemProduto;
-import br.com.qualquercoisa.ecommerce.repository.ImagemProdutoRepository;
 import br.com.qualquercoisa.ecommerce.service.ImagemProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ImagemProdutoController {
 
     @Autowired
-   private ImagemProdutoService imagemProdutoService;
+    private ImagemProdutoService imagemProdutoService;
 
-    @PostMapping("/imagemproduto")
-    public ResponseEntity<ImagemProduto> salvar(@RequestBody ImagemProduto ImagemProduto) {
-        return imagemProdutoService.salvar(ImagemProduto);
+    @PostMapping("/imagemProduto")
+    public ResponseEntity<ImagemProduto> salvar (@RequestBody ImagemProduto imagemProduto){
+        return imagemProdutoService.salvar(imagemProduto);
     }
+
+    @GetMapping("/imagemProduto")
+    public Iterable<ImagemProduto> listarTodos (){
+        return imagemProdutoService.listarTodos();
+    }
+
+    @GetMapping("/imagemProduto/{id}")
+    public ResponseEntity<ImagemProduto> buscarPorId(@PathVariable Long id){
+        return imagemProdutoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/imagemProduto/{id}")
+    public ResponseEntity deletar(@PathVariable Long id){
+        return imagemProdutoService.deletar(id);
+    }
+
+    @PutMapping("/imagemProduto/{id}")
+    public ResponseEntity<ImagemProduto> atualizar(
+            @PathVariable Long id,
+            @RequestBody ImagemProduto imagemProduto){
+        imagemProduto.setId(id);
+        return imagemProdutoService.salvar(imagemProduto);
+    }
+
 
 
 }
