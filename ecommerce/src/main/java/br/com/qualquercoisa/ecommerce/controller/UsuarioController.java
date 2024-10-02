e1ce043d79c58c25a57aab604623550ccbaf8de3
Laurindo Neto <laurindo1409@gmail.com>
2024-10-01 21:29:48 -0300

    Update UsuarioController.java

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/UsuarioController.java
@@ -1,14 +1,13 @@
 package br.com.qualquercoisa.ecommerce.controller;
 
+import br.com.qualquercoisa.ecommerce.entity.Categoria;
 import br.com.qualquercoisa.ecommerce.entity.Usuario;
 import br.com.qualquercoisa.ecommerce.repository.UsuarioRepository;
 import br.com.qualquercoisa.ecommerce.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsuarioController {
@@ -20,7 +19,27 @@ public class UsuarioController {
     @PostMapping("/usuario")
     public ResponseEntity<Usuario> salvar (@RequestBody Usuario usuario){
         return usuarioService.salvar(usuario);
+    }
+    @GetMapping("/usuario")
+    public Iterable<Usuario> listarTodos (){
+        return usuarioService.listarTodos();
+    }
 
+    @GetMapping("/usuario/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id){
+        return usuarioService.buscarPorId(id);
+    }
 
+    @DeleteMapping("/usuario/{id}")
+    public ResponseEntity deletar(@PathVariable Long id){
+        return usuarioService.deletar(id);
+    }
+
+    @PutMapping("/usuario/{id}")
+    public ResponseEntity<Usuario> atualizar(
+            @PathVariable Long id,
+            @RequestBody Usuario usuario){
+        usuario.setId(id);
+        return usuarioService.salvar(usuario);
     }
 }
