2c9ceb86175e5ea17b6103772a7dc15b8c9772db
Laurindo Neto <laurindo1409@gmail.com>
2024-10-01 21:29:44 -0300

    Update UsuarioService.java

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/UsuarioService.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/UsuarioService.java
@@ -1,10 +1,12 @@
 package br.com.qualquercoisa.ecommerce.service;
 
 import br.com.qualquercoisa.ecommerce.entity.Usuario;
+import br.com.qualquercoisa.ecommerce.entity.Usuario;
 import br.com.qualquercoisa.ecommerce.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,7 +16,18 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
 
     public ResponseEntity<Usuario> salvar (Usuario usuario){
-        Usuario usuarioJaSalva = usuarioRepository.save(usuario);
-        return new ResponseEntity<Usuario>(usuarioJaSalva, HttpStatus.OK);
+        return new ResponseEntity<Usuario>(usuarioRepository.save(usuario), HttpStatus.OK);
+    }
+
+    public Iterable<Usuario> listarTodos (){
+        return usuarioRepository.findAll();
+    }
+    public ResponseEntity<Usuario> buscarPorId(Long id) {
+        return new ResponseEntity<Usuario>(usuarioRepository.findById(id).orElseThrow(),HttpStatus.OK);
+    }
+
+    public ResponseEntity deletar(Long id) {
+        usuarioRepository.deleteById(id);
+        return new ResponseEntity("{\"mensagem\":\"Removido com sucesso\"}",HttpStatus.OK);
     }
 }
