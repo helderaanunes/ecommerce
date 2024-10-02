b7980a7dc11bb0261ae7a0a55a922a8ee71a0b4a
Your Name <you@example.com>
2024-10-01 21:02:13 -0300

    FluxoFinanceiro CRUD OK

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/FluxoFinanceiroService.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/FluxoFinanceiroService.java
@@ -1,5 +1,6 @@
 package br.com.qualquercoisa.ecommerce.service;
 
+import br.com.qualquercoisa.ecommerce.entity.Categoria;
 import br.com.qualquercoisa.ecommerce.entity.FluxoFinanceiro;
 import br.com.qualquercoisa.ecommerce.repository.FluxoFinanceiroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,24 @@ public class FluxoFinanceiroService {
     @Autowired
     private FluxoFinanceiroRepository fluxoFinanceiroRepository;
 
-    public ResponseEntity<FluxoFinanceiro> salvar (FluxoFinanceiro fluxoFinanceiro){
-        FluxoFinanceiro fluxoFinanceiroJaSalva = fluxoFinanceiroRepository.save(fluxoFinanceiro);
-        return new ResponseEntity<FluxoFinanceiro>(fluxoFinanceiroJaSalva, HttpStatus.OK);
+    public Iterable<FluxoFinanceiro> listarTodos (){
+        return fluxoFinanceiroRepository.findAll();
     }
+
+    public ResponseEntity<FluxoFinanceiro> salvar (FluxoFinanceiro fluxoFinanceiro){
+        return new ResponseEntity<FluxoFinanceiro>(fluxoFinanceiroRepository.save(fluxoFinanceiro), HttpStatus.OK);
+    }
+
+    public ResponseEntity<FluxoFinanceiro> buscarPorId(Long id) {
+        return new ResponseEntity<FluxoFinanceiro>(fluxoFinanceiroRepository.findById(id).orElseThrow(),HttpStatus.OK);
+    }
+
+    public ResponseEntity deletar(Long id) {
+        fluxoFinanceiroRepository.deleteById(id);
+        return new ResponseEntity("{\"mensagem\":\"Removido com sucesso\"}",HttpStatus.OK);
+    }
+
+
+
 
 }
