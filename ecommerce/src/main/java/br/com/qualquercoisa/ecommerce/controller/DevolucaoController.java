c0444146fd88da4e0fa6e9d8716ec28f1318237a
Your Name <Lucyysilva00@gmail.com>
2024-10-01 21:28:41 -0300

    crud devolucao

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/DevolucaoController.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/DevolucaoController.java
@@ -1,14 +1,14 @@
 package br.com.qualquercoisa.ecommerce.controller;
 
+import br.com.qualquercoisa.ecommerce.entity.Categoria;
 import br.com.qualquercoisa.ecommerce.entity.Devolucao;
 import br.com.qualquercoisa.ecommerce.repository.DevolucaoRepository;
+import br.com.qualquercoisa.ecommerce.service.CategoriaService;
 import br.com.qualquercoisa.ecommerce.service.DevolucaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DevolucaoController {
@@ -16,8 +16,31 @@ public class DevolucaoController {
     @Autowired
     private DevolucaoService devolucaoService;
 
-    @PostMapping ("/Devolucao")
-    public ResponseEntity<Devolucao> salvar(@RequestBody Devolucao devolucao){
+    @PostMapping("/devolucao")
+    public ResponseEntity<Devolucao> salvar (@RequestBody Devolucao devolucao){
+        return devolucaoService.salvar(devolucao);
+    }
+
+    @GetMapping("/devolucao")
+    public Iterable<Devolucao> listarTodos (){
+        return devolucaoService.listarTodos();
+    }
+
+    @GetMapping("/devolucao/{id}")
+    public ResponseEntity<Devolucao> buscarPorId(@PathVariable Long id){
+        return devolucaoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/devolucao/{id}")
+    public ResponseEntity deletar(@PathVariable Long id){
+        return devolucaoService.deletar(id);
+    }
+
+    @PutMapping("/devolucao/{id}")
+    public ResponseEntity<Devolucao> atualizar(
+            @PathVariable Long id,
+            @RequestBody Devolucao devolucao){
+        devolucao.setId(id);
         return devolucaoService.salvar(devolucao);
     }
 
--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/DevolucaoService.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/service/DevolucaoService.java
@@ -1,6 +1,8 @@
 package br.com.qualquercoisa.ecommerce.service;
 
+import br.com.qualquercoisa.ecommerce.entity.Categoria;
 import br.com.qualquercoisa.ecommerce.entity.Devolucao;
+import br.com.qualquercoisa.ecommerce.repository.CategoriaRepository;
 import br.com.qualquercoisa.ecommerce.repository.DevolucaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,15 +11,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
-
 public class DevolucaoService {
     @Autowired
     private DevolucaoRepository devolucaoRepository;
 
-    public ResponseEntity<Devolucao> salvar( Devolucao devolucao) {
-        Devolucao devolucaoJaSalva = devolucaoRepository.save(devolucao);
-        return new ResponseEntity<Devolucao>(devolucaoJaSalva, HttpStatus.OK);
+    public Iterable<Devolucao> listarTodos (){
+        return devolucaoRepository.findAll();
+    }
 
+    public ResponseEntity<Devolucao> salvar (Devolucao Devolucao){
+        return new ResponseEntity<Devolucao>(devolucaoRepository.save(Devolucao), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Devolucao> buscarPorId(Long id) {
+        return new ResponseEntity<Devolucao>(devolucaoRepository.findById(id).orElseThrow(),HttpStatus.OK);
+    }
+
+    public ResponseEntity deletar(Long id) {
+        devolucaoRepository.deleteById(id);
+        return new ResponseEntity("{\"mensagem\":\"Removido com sucesso\"}",HttpStatus.OK);
     }
 
 }
