1472b8738d8f5d797e33831a0dcf1ca0546a9c9e
Carlitoshenrique <chqc@discente.ifpe.edu.br>
2024-10-29 21:25:16 -0300

    Update EstoqueController.java

--- ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/EstoqueController.java
+++ ecommerce/src/main/java/br/com/qualquercoisa/ecommerce/controller/EstoqueController.java
@@ -1,9 +1,9 @@
 package br.com.qualquercoisa.ecommerce.controller;
 
-import br.com.qualquercoisa.ecommerce.entity.Categoria;
+import br.com.qualquercoisa.ecommerce.entity.Estoque;
 import br.com.qualquercoisa.ecommerce.entity.Estoque;
 //import br.com.qualquercoisa.ecommerce.repository.EstoqueRepository;
-import br.com.qualquercoisa.ecommerce.service.CategoriaService;
+import br.com.qualquercoisa.ecommerce.service.EstoqueService;
 import br.com.qualquercoisa.ecommerce.service.EstoqueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
